7b4ba40f4618e9eb1eafbf3aef1075748b6238f9
Straltsou Maxim <green1971weekend1@gmail.com>
2020-07-06 16:24:16 +0300

    feat: added three-layer structure

---
+++ Comander.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Comander.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Commander.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Commander.DAL
+{
+    public class Class1
+    {
+    }
+}
